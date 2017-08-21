d6455f96d77343e49ee43fd673b5910d8cf948d0
Lichun Dai <a546684355@qq.com>
2017-08-21 17:25:21 +0800

    update

--- karma.conf.js
+++ karma.conf.js
@@ -60,7 +60,7 @@ module.exports = function(config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['Chrome'],
+        browsers: ['PhantomJS'], //  Chrome
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
